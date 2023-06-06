3b6dbe1a77b11cee6df37bc514b9666d0da0ce2b
Mukai009 <129972942+Mukai009@users.noreply.github.com>
2023-06-06 08:05:36 +0100

    Add files via upload

---
+++ pages/api/editPost.js
@@ -0,0 +1,31 @@
+import clientPromise from "../../lib/mongodb";
+import { ObjectId } from "mongodb";
+
+export default async (req, res) => {
+  try {
+    const client = await clientPromise;
+    const db = client.db("posts");
+    const { id } = req.query;
+    const { title, content } = req.body;
+    console.log("edit post, ", id, title, content);
+
+    const post = await db.collection("posts").updateOne(
+      {
+        _id: ObjectId(id),
+      },
+      {
+        $set: {
+          title: title,
+          content: content,
+        },
+      }
+    );
+
+    console.log(post);
+
+    res.json(post);
+  } catch (e) {
+    console.error(e);
+    throw new Error(e).message;
+  }
+};
